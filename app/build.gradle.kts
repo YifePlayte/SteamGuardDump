410f378764768df7a8226312ab3f35a6e76106eb
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-07-02 13:50:37 +0800

    1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yifeplayte.steamguarddump"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         applicationVariants.configureEach {
             outputs.configureEach {
